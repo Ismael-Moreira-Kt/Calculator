41a4495289b17fc0ff6452b8a4c610e168dc64dd
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-15 18:36:41 +0100

    feat: Addition function that returns the multiply of two numbers.

--- src/calculator.ts
+++ src/calculator.ts
@@ -7,4 +7,9 @@ export class Calculator {
     static subtract(first_number: number, second_number: number): number {
         return first_number - second_number;
     }
+
+
+    static multiply(first_number: number, second_number: number): number {
+        return first_number * second_number;
+    }
 }
