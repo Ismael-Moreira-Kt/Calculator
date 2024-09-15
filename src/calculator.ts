21ceac80b77558fd7d68cf9bc239c0a0ac1df5b3
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-15 18:37:21 +0100

    feat: Addition function that returns the divide of two numbers.

--- src/calculator.ts
+++ src/calculator.ts
@@ -12,4 +12,13 @@ export class Calculator {
     static multiply(first_number: number, second_number: number): number {
         return first_number * second_number;
     }
+
+
+    static divide(first_number: number, second_number: number): number {
+        if (second_number === 0) {
+            throw new Error("Division by zero is not allowed.");
+        }
+        
+        return first_number / second_number;
+    }
 }
