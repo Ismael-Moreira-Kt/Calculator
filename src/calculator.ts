1150275344cb544fa1c40a89e803d1fb6566e7be
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-15 18:35:47 +0100

    feat: Addition function that returns the subtract of two numbers.

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,4 +2,9 @@ export class Calculator {
     static add(first_number: number, second_number: number): number {
         return first_number + second_number;
     }
-}  
+
+
+    static subtract(first_number: number, second_number: number): number {
+        return first_number - second_number;
+    }
+}
