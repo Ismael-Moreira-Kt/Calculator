e023bf41b7fca0c7a8ef9a5db1af74e83aebada9
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-15 18:51:09 +0100

    feat: Add function to get a number from user input.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,25 @@ import { Calculator } from './calculator';
 import readline from 'readline';
 
 
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+
+
+function getNumber(question: string): Promise<number> {
+    return new Promise((resolve) => {
+        rl.question(question, (input) => {
+            const number = parseFloat(input);
+
+            if (!isNaN(number)) {
+                resolve(number);
+            } else {
+                console.log('Please enter a valid number.');
+                resolve(getNumber(question));
+            }
+        });
+    });
+}
